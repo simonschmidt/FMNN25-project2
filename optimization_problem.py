6bf23f0b91567a6e3db54ad8e5fe300756b7f2cb
Simon Schmidt <schmidt.simon@gmail.com>
2012-10-02 16:02:40 +0200

    starting point and some comments

--- optimization_problem.py
+++ optimization_problem.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
+#coding: utf8
 import scipy
 import pylab
 
 class OptimizationProblem(object):
     """ ??? """
-    def __init__(self, f, gradient = None):
+    def __init__(self, f, shape, gradient = None):
         """Solves an optimization problem, except that it doesn't
 
         Arguments:
         f -- the function
         gradient -- the gradient (default to numerical approximation)
+        shape -- Dimension of input argument to function
+                 (if f: ℝⁿ->ℝ then shape=n)
         """
 
         self.f = f
@@ -19,21 +22,37 @@ class OptimizationProblem(object):
             def df(x):
                 return scipy.derivative(self.f,x)
             self.gradient = df
+        self.shape = shape
 
-    def argmax(self,start=None,range=None):
-        """Finds the input that gives the maximum value of f"""
+    def argmax(self,start=None):
+        """Finds the input that gives the maximum value of f
+            Optional arguments:
+                start: Starting point
+                range: Search range
+        """
         pass
 
-    def argmin(self,start=None,range=None):
-        """Finds the input that gives the minimum value of f"""
+    def argmin(self,start=None):
+        """Finds the input that gives the minimum value of f
+            Optional arguments:
+                start: Starting point
+                range: Search range
+        """
         pass
 
-    def max(self,start=None,range=None):
-        """Finds the maximum of f"""
+    def max(self,start=None):
+        """Finds the maximum of f
+            Optional arguments:
+                start: Starting point
+                range: Search range
+        """
 
-        return self.f(self.argmax())
+        return self.f(self.argmax(),start)
 
-    def min(self,start=None,range=None):
-        """Finds the minimum of f"""
-
-        return self.f(self.argmin())
+    def min(self,start=None):
+        """Finds the minimum of f
+            Optional arguments:
+                start: Starting point
+                range: Search range
+        """
+        return self.f(self.argmin(), start)
