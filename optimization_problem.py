363c745de1f69b0f830fd89ad7280ef38318c120
Jens Nockert <jens@nockert.se>
2012-10-02 15:35:05 +0200

    Initial interface?

---
+++ optimization_problem.py
@@ -0,0 +1,31 @@
+import scipy
+import pylab
+
+class OptimizationProblem(object):
+    """ ??? """
+    def __init__(self, f, gradient = None):
+        """Solves an optimization problem, except that it doesn't
+
+        Arguments:
+        f -- the function
+        gradient -- the gradient (default to ???)
+        """
+
+        self.f = f
+        self.gradient = gradient
+
+    def argmax():
+        """Finds the input that gives the maximum value of f"""
+
+    def argmin():
+        """Finds the input that gives the minimum value of f"""
+
+    def max():
+        """Finds the maximum of f"""
+
+        return self.f(self.argmax())
+
+    def min():
+        """Finds the minimum of f"""
+
+        return self.f(self.argmin())
